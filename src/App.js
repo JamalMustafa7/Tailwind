45d9b9e4f8b52e799d663e9baff6b952c8e0c7a1
JamalMustafa7 <muhammedjamalmustafa@gmail.com>
2024-08-27 11:21:11 +0500

    Hover effects on a button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import MyCard from './components/MyCard'
 function App() {
+
   return (
-    <div className="App h-screen bg-slate-900 p-4">
+    <div className="App h-screen bg-slate-900 p-4 grid place-content-center place-items-center">
+      <MyCard />
       
     </div>
   );
---
+++ src/components/HoverButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HoverButton = () => {
+  return (
+    <button className="bg-blue-500 p-2 text-white rounded-lg my-3 w-max hover:bg-white hover:text-black transition-all duration-300">
+        Buy Now
+      </button>
+  )
+}
+
+export default HoverButton
